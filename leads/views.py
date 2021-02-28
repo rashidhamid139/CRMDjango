0822009a8e455753134abd3b78b3f4b686382c1c
rashidhamid139 <rashidhamid139@gmail.com>
2021-02-28 15:17:52 +0530

    created an agent and lead models for leads app

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home_page(request):
+    context = {
+        "name": "joe",
+        "age": 35
+    }
+    return render(request, 'leads/home_page.html', context)
